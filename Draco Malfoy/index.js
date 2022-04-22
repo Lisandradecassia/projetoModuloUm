42afc855bcda18fc9966eb14339210afbc919c19
lisandradecassia <lisandradecassia@gmail.com>
2022-04-22 01:10:01 -0300

    Primeiro commit

---
+++ Draco Malfoy/index.js
@@ -0,0 +1,23 @@
+function faseDois() {
+    var contador = 0;
+    while (true) {
+        var alternativa = (prompt("Digite a alternativa correta (a,b ou c)!").toLowerCase());
+
+        if (alternativa == 'b') {
+            alert('Mandou bem, Sonserino! Vamos para a fase 2?');
+            location.assign("../Fase2/index.html")
+            break;
+        } else if (alternativa == 'a' || alternativa == 'c') {
+            if (contador >= 1) {
+                location.assign("../Perdeu/index.html")
+                break;
+            } else {
+                alert('Alternativa incorreta!');
+                contador++;
+            }
+
+        } else {
+            alert("Você deverá digitar apenas a, b ou c!")
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+function telaDois() {
+  location.assign("./telaDois/index.html");
+}
+function telaTres() {
+  location.assign("../telaTres/index.html");
+}
+function personagens() {
+  location.assign("../telaTres/index.html");
+}
+function inicio() {
+  location.assign("../index.html");
+}
+function draco() {
+  location.assign("../Draco Malfoy/Fase1/index.html");
+}
+function voldemort() {
+  location.assign("../Lord Woldemort/Fase1/index.html");
+}
+function snape() {
+  location.assign("../Severo Snape/Fase1/index.html");
+}
